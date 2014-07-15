a73589fa0c23a4217195a6eabdea77c4977adfbd
millken <millken@gmail.com>
2014-07-15 15:56:48 +0800

    add raw dns

--- skill/raw-dns.go
+++ skill/raw-dns.go
@@ -1,239 +1,92 @@
-/*
-http://blog.csdn.net/gophers/article/details/20393601
-https://github.com/grahamking/latency/blob/master/latency.go
-http://www.51testing.com/html/66/138366-216709.html IP包头结构详解
-*/
+//http://blog.csdn.net/gophers/article/details/22942457
 package main
+
 import (
-	"flag"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
-	"os"
-	"strconv"
 	"strings"
-	"syscall"
-)
-const (
-        Version      = 4  // protocol version
-        HeaderLen    = 20 // header length without extension headers
-        maxHeaderLen = 60 // sensible default, revisit if later RFCs define new usage of version and header length fields
-)
-const (
-        posTOS      = 1  // type-of-service
-        posTotalLen = 2  // packet total length
-        posID       = 4  // identification
-        posFragOff  = 6  // fragment offset
-        posTTL      = 8  // time-to-live
-        posProtocol = 9  // next protocol
-        posChecksum = 10 // checksum
-        posSrc      = 12 // source address
-        posDst      = 16 // destination address
 )
 
-const supportsNewIPInput = runtime.GOOS == "linux" || runtime.GOOS == "openbsd"
-
-var (
-	domainFlag   = flag.String("domain", "www.google.com", "checkdomain, default is : www.google.com")
-)
-type HeaderFlags int
-
-const (
-        MoreFragments HeaderFlags = 1 << iota // more fragments flag
-        DontFragment                          // don't fragment flag
-)
-type IpHeader struct {
-    Version  int         // protocol version
-    Len      int         // header length
-    TOS      int         // type-of-service
-    TotalLen int         // packet total length
-    ID       int         // identification
-    Flags    int // flags
-    FragOff  int         // fragment offset
-    TTL      int         // time-to-live
-    Protocol int         // next protocol
-    Checksum int         // checksum
-    Src      net.IP      // source address
-    Dst      net.IP      // destination address
-    Options  []byte      // options, extension headers
-}
-
-type UdpHdr struct {
-	Source	int
-	Dest	int
-	Len	int
-	Check 	int
-}
-
-type PsdHeader struct {
-	SrcAddr	uint32
-	DstAddr	uint32
-	Filler	uint8
-	Protocol	uint8
-	Len	uint16
-}
 type DNSHeader struct {
-	ID uint16
-	Flag uint16
+	ID            uint16
+	Flag          uint16
 	QuestionCount uint16
-	AnswerRRs uint16 //RRs is Resource Records
-	AuthorityRRs uint16
+	AnswerRRs     uint16 //RRs is Resource Records
+	AuthorityRRs  uint16
 	AdditionalRRs uint16
 }
-func htons(port uint16) uint16 {  
-    var (  
-        high uint16 = port >> 8  
-        ret  uint16 = port<<8 + high  
-    )  
-    return ret  
-}  
-func CheckSum(data []byte) uint16 {  
-    var (  
-        sum    uint32  
-        length int = len(data)  
-        index  int  
-    )  
-    for length > 1 {  
-        sum += uint32(data[index])<<8 + uint32(data[index+1])  
-        index += 2  
-        length -= 2  
-    }  
-    if length > 0 {  
-        sum += uint32(data[index])  
-    }  
-    sum += (sum >> 16)  
-  
-    return uint16(^sum)  
-}  
-
-func ipStringToI32(a string) uint32 {
-	return ipToI32(net.ParseIP(a))
-}
-func ipToI32(ip net.IP) uint32 {
-	ip = ip.To4()
-	return uint32(ip[0])<<24 | uint32(ip[1])<<16 | uint32(ip[2])<<8 | uint32(ip[3])
-}
-
-func i32ToIP(a uint32) net.IP {
-	return net.IPv4(byte(a>>24), byte(a>>16), byte(a>>8), byte(a))
-}
-
-func inet_addr(ipaddr string) uint32 {  
-    var (  
-        segments []string = strings.Split(ipaddr, ".")  
-        ip       [4]uint64  
-        ret      uint64  
-    )  
-    for i := 0; i < 4; i++ {  
-        ip[i], _ = strconv.ParseUint(segments[i], 10, 64)  
-    }  
-    ret = ip[3]<<24 + ip[2]<<16 + ip[1]<<8 + ip[0]  
-    return uint32(ret)  
-}  
 
 func (header *DNSHeader) SetFlag(QR uint16, OperationCode uint16, AuthoritativeAnswer uint16, Truncation uint16, RecursionDesired uint16, RecursionAvailable uint16, ResponseCode uint16) {
 	header.Flag = QR<<15 + OperationCode<<11 + AuthoritativeAnswer<<10 + Truncation<<9 + RecursionDesired<<8 + RecursionAvailable<<7 + ResponseCode
 }
 
 type DNSQuery struct {
-	QuestionType uint16
+	QuestionType  uint16
 	QuestionClass uint16
 }
-func (h *IpHeader) Marshal() ([]byte, error) {
-        if h == nil {
-                return nil, syscall.EINVAL
-        }
-        if h.Len < HeaderLen {
-                return nil, errHeaderTooShort
-        }
-        hdrlen := HeaderLen + len(h.Options)
-        b := make([]byte, hdrlen)
-        b[0] = byte(Version<<4 | (hdrlen >> 2 & 0x0f))
-        b[posTOS] = byte(h.TOS)
-        flagsAndFragOff := (h.FragOff & 0x1fff) | int(h.Flags<<13)
-        if supportsNewIPInput {
-                b[posTotalLen], b[posTotalLen+1] = byte(h.TotalLen>>8), byte(h.TotalLen)
-                b[posFragOff], b[posFragOff+1] = byte(flagsAndFragOff>>8), byte(flagsAndFragOff)
-        } else {
-                *(*uint16)(unsafe.Pointer(&b[posTotalLen : posTotalLen+1][0])) = uint16(h.TotalLen)
-                *(*uint16)(unsafe.Pointer(&b[posFragOff : posFragOff+1][0])) = uint16(flagsAndFragOff)
-        }
-        b[posID], b[posID+1] = byte(h.ID>>8), byte(h.ID)
-        b[posTTL] = byte(h.TTL)
-        b[posProtocol] = byte(h.Protocol)
-        b[posChecksum], b[posChecksum+1] = byte(h.Checksum>>8), byte(h.Checksum)
-        if ip := h.Src.To4(); ip != nil {
-                copy(b[posSrc:posSrc+net.IPv4len], ip[:net.IPv4len])
-        }
-        if ip := h.Dst.To4(); ip != nil {
-                copy(b[posDst:posDst+net.IPv4len], ip[:net.IPv4len])
-        } else {
-                return nil, errMissingAddress
-        }
-        if len(h.Options) > 0 {
-                copy(b[HeaderLen:], h.Options)
-        }
-        return b, nil
-}
-/*
-// Pseudoheader struct
-typedef struct
-{
-    u_int32_t saddr;
-    u_int32_t daddr;
-    u_int8_t filler;
-    u_int8_t protocol;
-    u_int16_t len;
-}ps_hdr;
 
-// DNS header struct
-typedef struct
-{
-	unsigned short id; 		// ID
-	unsigned short flags;	// DNS Flags
-	unsigned short qcount;	// Question Count
-	unsigned short ans;		// Answer Count
-	unsigned short auth;	// Authority RR
-	unsigned short add;		// Additional RR
-}dns_hdr;
-
-// Question types
-typedef struct
-{
-	unsigned short qtype;
-	unsigned short qclass;
-}query;
-*/
-
-func init() {
-	flag.Parse()
-	// Increase file descriptor limit
-	rlimit := syscall.Rlimit{Max: uint64(500000), Cur: uint64(500000)}
-	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
-		fmt.Fprintf(os.Stderr, "Error setting rlimit: %s", err)
+func ParseDomainName(domain string) []byte {
+	//要将域名解析成相应的格式，例如：
+	//"www.google.com"会被解析成"0x03www0x06google0x03com0x00"
+	//就是长度+内容，长度+内容……最后以0x00结尾
+	var (
+		buffer   bytes.Buffer
+		segments []string = strings.Split(domain, ".")
+	)
+	for _, seg := range segments {
+		binary.Write(&buffer, binary.BigEndian, byte(len(seg)))
+		binary.Write(&buffer, binary.BigEndian, []byte(seg))
 	}
+	binary.Write(&buffer, binary.BigEndian, byte(0x00))
+
+	return buffer.Bytes()
 }
 
 func main() {
-	ipheader := IpHeader{
-		Version:	4,
-		Len      0,         // header length
-		TOS:	0,
-		TotalLen:	0,
-		ID:	12345,
-		Flags:	DontFragment,
-		FragOff:	0,
-		TTL:	64,
-		Protocol:	syscall.IPPROTO_UDP,
-		Checksum:	0,
-		Src:      net.ParseIP("127.0.0.2"),
-		Dst:	net.ParseIP("127.0.0.1"),
-		Options  []byte{}
+	var (
+		dns_header   DNSHeader
+		dns_question DNSQuery
+	)
+
+	//填充dns首部
+	dns_header.ID = 0xFFFF
+	dns_header.SetFlag(0, 0, 0, 0, 1, 0, 0)
+	dns_header.QuestionCount = 1
+	dns_header.AnswerRRs = 0
+	dns_header.AuthorityRRs = 0
+	dns_header.AdditionalRRs = 0
+
+	//填充dns查询首部
+	dns_question.QuestionType = 1  //IPv4
+	dns_question.QuestionClass = 1  
+
+	var (
+		conn net.Conn
+		err  error
+
+		buffer bytes.Buffer
+	)
+
+	//DNS服务器的端口一般是53，IP你自己ipconfig查一下
+	//别忘了DNS是基于UDP协议的
+	if conn, err = net.Dial("udp", "127.0.0.1:53"); err != nil {
+		fmt.Println(err.Error())
+		return
 	}
-	psdheader := PsdHeader{
-		SrcAddr:	inet_addr("127.0.0.2"),
-		DstAddr:	inet_addr("127.0.0.1"),
-		Filler:	0,
-		Protocol:	syscall.IPPROTO_UDP,
-		Len:	htons(sizeof(udph) + sizeof(dns_hdr) + (strlen(dns_name)+1) + sizeof(query)),
+	defer conn.Close()
+
+	//buffer中是我们要发送的数据，里面的内容是DNS首部+查询内容+DNS查询首部
+	binary.Write(&buffer, binary.BigEndian, dns_header)
+	binary.Write(&buffer, binary.BigEndian, ParseDomainName("www.baidu.com"))
+	binary.Write(&buffer, binary.BigEndian, dns_question)
+	fmt.Println(buffer.Bytes())
+
+	if _, err := conn.Write(buffer.Bytes()); err != nil {
+		fmt.Println(err.Error())
+		return
 	}
+	fmt.Println("send success.")
 }
+
---
+++ skill/raw-udp-dns.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+    "bytes"
+    "encoding/binary"
+    . "fmt"
+    "strconv"
+    "strings"
+    "syscall"
+    "unsafe"
+)
+
+type IpHeader struct {
+    Version  int         // protocol version
+    Len      int         // header length
+    TOS      int         // type-of-service
+    TotalLen int         // packet total length
+    ID       int         // identification
+    //Flags    int // flags
+    FragOff  int         // fragment offset
+    TTL      int         // time-to-live
+    Protocol int         // next protocol
+    Checksum int         // checksum
+    Src      net.IP      // source address
+    Dst      net.IP      // destination address
+    //Options  []byte      // options, extension headers
+}
+
+type UdpHdr struct {
+	Source	int
+	Dest	int
+	Len	int
+	Check 	int
+}
+
+type PsdHeader struct {
+	SrcAddr	uint32
+	DstAddr	uint32
+	Filler	uint8
+	Protocol	uint8
+	Len	uint16
+}
+type DNSHeader struct {
+	ID uint16
+	Flag uint16
+	QuestionCount uint16
+	AnswerRRs uint16 //RRs is Resource Records
+	AuthorityRRs uint16
+	AdditionalRRs uint16
+}
+
+type Query struct
+{
+	Qtype uint16
+	Qclass uint16
+}
+
+type TCPHeader struct {
+    SrcPort   uint16
+    DstPort   uint16
+    SeqNum    uint32
+    AckNum    uint32
+    Offset    uint8
+    Flag      uint8
+    Window    uint16
+    Checksum  uint16
+    UrgentPtr uint16
+}
+
+
+func inet_addr(ipaddr string) uint32 {
+    var (
+        segments []string = strings.Split(ipaddr, ".")
+        ip       [4]uint64
+        ret      uint64
+    )
+    for i := 0; i < 4; i++ {
+        ip[i], _ = strconv.ParseUint(segments[i], 10, 64)
+    }
+    ret = ip[3]<<24 + ip[2]<<16 + ip[1]<<8 + ip[0]
+    return uint32(ret)
+}
+
+func htons(port uint16) uint16 {
+    var (
+        high uint16 = port >> 8
+        ret  uint16 = port<<8 + high
+    )
+    return ret
+}
+
+func CheckSum(data []byte) uint16 {
+	var (
+		sum    uint32
+		length int = len(data)
+		index  int
+	)
+	for length > 1 {
+		sum += uint32(data[index])<<8 + uint32(data[index+1])
+		index += 2
+		length -= 2
+	}
+	if length > 0 {
+		sum += uint32(data[index])
+	}
+	sum += (sum >> 16)
+
+	return uint16(^sum)
+}
+
+func main() {
+    var (
+        msg       string
+        psdheader PsdHeader
+        ipheader  IpHeader
+        dnsheader DNSHeader
+        query     Query
+    )
+
+
+	dnsheader.ID = 123
+	dnsheader.Flag = htons(0x0100)
+	dnsheader.QuestionCount = htons(1)
+	dnsheader.AnswerRRs = 0
+	dnsheader.AuthorityRRs = 0
+	dnsheader.AdditionalRRs = 0
+
+	query->Qtype = htons(0x00ff);
+	query->Qclass = htons(0x1);
+	
+    /*填充UDP伪首部*/
+    psdheader.SrcAddr = inet_addr("192.168.3.200")
+    psdheader.DstAddr = inet_addr("123.125.114.144")
+    psdheader.Filler = 0
+    psdheader.Protocol = syscall.IPPROTO_UDP
+    psdheader.Len = uint16(unsafe.Sizeof(UdpHdr{})) + uint16(len(msg))
+    //htons(sizeof(udph) + sizeof(dns_hdr) + (strlen(dns_name)+1) + sizeof(query));
+
+    /*填充IP首部*/
+    ipheader.Version = 4
+	ipheader.Len = 5         // header length
+	ipheader.TOS = 0
+	ipheader.TotalLen =	0
+	ipheader.ID = 12345
+	//ipheader.Flags:	DontFragment,
+	ipheader.FragOff = 0
+	ipheader.TTL = 64
+	ipheader.Protocol = syscall.IPPROTO_UDP
+	ipheader.Checksum =	0
+	ipheader.Src = net.ParseIP("127.0.0.2")
+	ipheader.Dst = net.ParseIP("127.0.0.1")
+	//ipheader.Options = []byte{}
+
+    /*buffer用来写入两种首部来求得校验和*/
+    var (
+        buffer bytes.Buffer
+    )
+    binary.Write(&buffer, binary.BigEndian, psdheader)
+    binary.Write(&buffer, binary.BigEndian, ipheader)
+    tcpheader.Checksum = CheckSum(buffer.Bytes())
+
+    /*接下来清空buffer，填充实际要发送的部分*/
+    buffer.Reset()
+    binary.Write(&buffer, binary.BigEndian, tcpheader)
+    binary.Write(&buffer, binary.BigEndian, msg)
+
+
+	
+    /*下面的操作都是raw socket操作，大家都看得懂*/
+    var (
+        sockfd int
+        addr   syscall.SockaddrInet4
+        err    error
+    )
+    if sockfd, err = syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_UDP); err != nil {
+        Println("Socket() error: ", err.Error())
+        return
+    }
+    defer syscall.Shutdown(sockfd, syscall.SHUT_RDWR)
+    addr.Addr[0], addr.Addr[1], addr.Addr[2], addr.Addr[3] = 127, 0, 0, 0
+    addr.Port = 53
+    if err = syscall.Sendto(sockfd, buffer.Bytes(), 0, &addr); err != nil {
+        Println("Sendto() error: ", err.Error())
+        return
+    }
+    Println("Send success!")
+}
+
